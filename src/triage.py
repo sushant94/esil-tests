77cdf8469e6c9794edc5b4343cb41b62a988a25c
Sushant <sushant.dinesh94@gmail.com>
2016-03-26 01:55:54 +0530

    Adds reported and ignore lists

--- src/triage.py
+++ src/triage.py
@@ -9,11 +9,11 @@ Some basic functions that it currently supports are:
 TODO:
     - Blacklist / Skip offsets.
     - Template full bug-reporting. Github API to automatically open this bug.
-    - Generate testcase file for use in r2-regressions automatically.
 '''
 
 import sys
 import json
+import subprocess
 
 def analyze_freq(h):
     freq = {}
@@ -26,20 +26,41 @@ def analyze_freq(h):
     return freq
 
 def pretty_print(r):
+    # TODO
+    print "Binary: Ubuntu 14.04 /bin/ls"
     print "Instruction: " + r["instruction"]["opcode"]
     print "ESIL: " + r["instruction"]["esil"]
     print "Offset: " + r["instruction"]["offset"]
-    print "+" + "-"*74 + "+"
-    print "| REGISTER".ljust(25) + "| GDB".ljust(25) + "| ESIL".ljust(25) + "|"
-    print "+" + "-"*74 + "+"
+    rwd = 10
+    vwd = 20
+    width = rwd + 4*vwd
+    #print "+" + "-"*(width-1) + "+"
+    print
+    print "| REGISTER".ljust(rwd) + "| GDB".ljust(2*vwd) + "| ESIL".ljust(2*vwd) + "|"
+    print "|" + "-"*(rwd-1) + "|" + "-"*(2*vwd-1) + "|" + "-"*(2*vwd-1) + "|"
     for d in r["diff"]:
-        print "| {} | {} | {} |".format(d.ljust(22), r["diff"][d]["gdb"].ljust(22), r["diff"][d]["esil"].ljust(22))
-    print "+" + "-"*74 + "+"
+        gdb_s = "{} -> {}".format(r["diff"][d]["gdb_old"], r["diff"][d]["gdb"])
+        esil_s = "{} -> {}".format(r["diff"][d]["esil_old"], r["diff"][d]["esil"])
+        print "| {} | {} | {} |".format(d.ljust(rwd-3), gdb_s.ljust(2*vwd - 3),\
+                esil_s.ljust(2*vwd - 3))
+    print "|" + "-"*(rwd-1) + "|" + "-"*(2*vwd-1) + "|" + "-"*(2*vwd-1) + "|"
+    print
 
 def get_next(i, freq):
     it = freq.items()
     it.sort(key=lambda item: (len(item[1]), item[1]), reverse=True)
     return it[i][1]
+
+def get_offset(e):
+    return e["instruction"]["offset"]
+
+
+def update_reports(f, arr, elem):
+    # Update the reported list
+    offset = get_offset(elem)
+    arr.append(offset)
+    f.write(offset + "\n")
+
 
 if __name__ == "__main__":
     logfile = sys.argv[1]
@@ -47,10 +68,16 @@ if __name__ == "__main__":
     h = json.load(f)
     print "Analyzing ...."
     freq = analyze_freq(h)
+    # Get offsets of mis-matches that have already been reported.
+    report_f = open("reports", 'a+')
+    reported = open("reports", 'r').read().split("\n")
 
-    print '''
+    print '''* - Requires GHI and Authentication
              [N] Next occurance of mismatch of same instruction
              [S] Next Mismatched instruction (sorted based on frequency)
+             [I] Ignore this mismatch
+             [O] Open an Issue [*]
+             [C] Add a comment to an existing issue [*]
              [Q] Quit
            '''
     option = "S"
@@ -58,6 +85,10 @@ if __name__ == "__main__":
     fiter = -1
     iiter = -1
     cur_l = []
+
+    print "Total Mismatches: " + str(len(h))
+    print "Reported / Ignored: " + str(len(reported))
+    print
 
     while True:
         if option == "S":
@@ -67,11 +98,29 @@ if __name__ == "__main__":
             iiter = -1
         elif option == "N" or option == "":
             iiter += 1
+            while iiter < len(cur_l) and get_offset(cur_l[iiter]) in reported:
+                iiter += 1
+                continue
             if iiter < len(cur_l):
                 pretty_print(cur_l[iiter])
             else:
                 print "No more to display."
+        elif option == "O":
+            # Opens a bug with assignee as self, and labeled as "esil" and "blocker"
+            subprocess.call("ghi open --claim --label esil blocker", shell=True)
+            update_reports(report_f, reported, cur_l[iiter])
+            # Goto next mis-match
+            iiter += 1
+        elif option == "C":
+            issue = raw_input().strip()
+            subprocess.call("ghi comment" + issue)
+            update_reports(report_f, reported, cur_l[iiter])
+            iiter += 1
+        elif option == "I":
+            update_reports(report_f, reported, cur_l[iiter])
+            iiter += 1
         else:
+            report_f.close()
             break
         print "Command: ",
         option = raw_input().strip()
